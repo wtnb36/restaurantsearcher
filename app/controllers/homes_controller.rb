f74b94d3865a9926e593882e791b95cba77b1337
wtnb36 <miguel03061017@gmail.com>
2021-06-28 22:03:20 +0900

    update

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,8 +1,8 @@
 class HomesController < ApplicationController
   def top
     # ランダムで表示 mySQLではRANDへ変更
-    #@restaurants = Restaurant.all.order('RANDOM()').limit(5)
-    @restaurants = Restaurant.all.order("RAND()").limit(5)
+    @restaurants = Restaurant.all.order('RANDOM()').limit(5)
+    #@restaurants = Restaurant.all.order("RAND()").limit(5)
   end
 
   def about; end
--- app/controllers/public/favorites_controller.rb
+++ app/controllers/public/favorites_controller.rb
@@ -1,16 +1,16 @@
 class Public::FavoritesController < ApplicationController
   before_action :authenticate_customer!
   def create
-    restaurant = Restaurant.find(params[:restaurant_id])
-    favorite = current_customer.favorites.new(restaurant_id: restaurant.id)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    favorite = current_customer.favorites.new(restaurant_id: @restaurant.id)
     favorite.save
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
   def destroy
-    restaurant = Restaurant.find(params[:restaurant_id])
-    favorite = current_customer.favorites.find_by(restaurant_id: restaurant.id)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    favorite = current_customer.favorites.find_by(restaurant_id: @restaurant.id)
     favorite.destroy
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 end
